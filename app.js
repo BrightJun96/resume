7171a81be3d7fc43be5f00c874efff93eb042e2a
KIM CHEOL JUN <cjfwns966@gmail.com>
2022-05-27 01:24:53 +0900

    리팩토링

--- app.js
+++ app.js
@@ -1,129 +1,43 @@
-// modalContent
+/*--modalContent--*/
 const modalContent = [
   {
     title: "블록체인 기술에 대한 관심과 이해",
     description:
       "블록체인 서비스에 대한 관심으로 블록체인 관련 지식을 정리하였고 현재 react, typescript, redux기반으로 블록체인 개인 프로젝트를 개발하고 있으며 NFT 관련 팀 프로젝트에 프론트엔드 포지션으로 활동예정입니다.<br/><br/>회사 관련 서비스(NFT 마켓 플레이스 및 Definix)에 관하여 쉽고 간편하게 접근할 수 있도록 UI 및 기능,성능 개선 등에 기여하여 유저에게 만족스러운 서비스를 제공하고 싶습니다.<br/><br/>이를 위해 프론트엔드 개발자로서 이용객들이 다소 어려울 수 있는 블록체인 서비스에 쉽고 편리하게 접근할 수 있도록 하는 것이 목표입니다.",
-    link: [
-      {
-        href: "https://airy-basin-cb3.notion.site/e3b917d6e5c7409eb0afa7eb5eebb827",
-        text: "블록체인 개념 정리한 글",
-      },
-      {
-        href: "https://github.com/BrightJun96/BlockChain",
-        text: "블록체인 프로젝트 코드",
-      },
-    ],
   },
   {
     title: "React, TypeScript 개발 경험",
     description:
       "React, TypeScript기반 미니프로젝트를 개발한 경험이 있으며 현재 블록체인 프로젝트를 React, TypeScript, Redux기반으로 진행중입니다.<br/>React에 대한 다양한 기술 스택에 대하여  React Repository에 기록하고 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/react-project/tree/main/blog_project",
-        text: "React 기반 블로그 프로젝트",
-      },
-      {
-        href: "https://github.com/BrightJun96/TodoList-react-with-TypeScript-",
-        text: "TodoList Project",
-      },
-      {
-        href: "https://github.com/BrightJun96/catch-mole-game",
-        text: "Catch-mole Project",
-      },
-      {
-        href: "https://github.com/BrightJun96/React",
-        text: "React Repository",
-      },
-    ],
   },
   {
     title: "Redux 상태 관리",
     description:
       "Redux를 사용하여 리액트 기반 블로그 프로젝트에서 각 페이지에 관련한 복잡한 전역 상태들을 카테고리별로 분류하여  체계적인 상태 관리를 한 경험이 있으며 현재 진행중인 블록체인 프로젝트 또한 Redux를 사용하여 상태관리를 하고 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/react-project/tree/main/blog_project/blog-frontend/src/modules",
-        text: "블로그 프로젝트 상태관리 코드",
-      },
-      {
-        href: "https://github.com/BrightJun96/BlockChain/tree/main/src/slices",
-        text: "블록체인 프로젝트 상태관리 코드",
-      },
-    ],
   },
   {
     title: "Html, Css, JavaScript(ES6)",
     description:
       "HTML에 관하여 UI에 적절한 시맨틱 태그를 설정할 수 있습니다.<br/>CSS에 관하여 Styled-Components, Sass와 같은 Styling기술들을 활용할 줄 알며 스타일링을 하기 위해 여러 방법을 제시한 뒤 최선의 스타일링 방법을 선택합니다.<br/><br/>JavaScript(ES6)에 관하여 최신 자바스크립트 문법을 기반으로 spread , async/await , fetch , ajax, 클로저 , 브라우저 렌더링 과정 등 자바스크립트에 대한 이해도 및 활용 경험을 가지고 있습니다.<br/><br/>라이브러리,프레임워크없이 HTML , CSS , JavaScript로 포트폴리오를 개발한 경험이 있으며 자바스크립트 지식 관련하여 정리해놓은  자바스크립트 레포지터리와 인터뷰 레포지터리가 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/PortPolio",
-        text: "Portpolio Repository",
-      },
-      {
-        href: "https://github.com/BrightJun96/Interview",
-        text: "Interview Repository       ",
-      },
-      {
-        href: "https://github.com/BrightJun96/JavaScript",
-        text: "Javascript Repository",
-      },
-      ,
-      ,
-      ,
-    ],
   },
   {
     title: "REST API에 대한 이해",
     description:
       "REST API에 대한 이해와 블로그 프로젝트에서 REST API를 활용하여 블로그 포스팅에 대한 CRUD를 구현한 경험이 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/JavaScript/tree/main/REST API",
-        text: "REST API에 대한 이해",
-      },
-      {
-        href: "https://github.com/BrightJun96/react-project/tree/main/blog_project/blog-frontend/src/lib/api",
-        text: "블로그 프로젝트 REST API 구성 코드",
-      },
-      ,
-      ,
-    ],
   },
   {
     title: "반응형 디자인",
     description:
       "각종 디바이스에 맞춰 대응할 수 있도록 반응형으로 웹 프로젝트를 디자인한 경험이 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/react-project/tree/main/mbti_project/src/styles",
-        text: "MBTI 프로젝트 스타일링 코드",
-      },
-      {
-        href: "https://github.com/BrightJun96/react-project/tree/main/card_project/src/styles",
-        text: "Hot Place 프로젝트 스타일링 코드",
-      },
-    ],
   },
   {
     title: "문제 해결 방식",
     description:
       "프로젝트 및 코드 관련 문제가 있다면 문제에 대하여 다음과 같은 프로세스를 가지고 해결합니다. <br/><ol><li>어떤 문제에 대하여 발생한 원인을 여러 가지로 나열하여 기록합니다.</li><li>각 추측원인에 대응한 해결방법을 제시하여 기록합니다.</li><li>각 원인에 대한 해결방법을 차례로 시도해봅니다.</li><br/> 위와 같은 프로세스로 프로젝트 이슈 및 문제 등을 깃헙 이슈탭을 이용하여 관리하고 있습니다.",
-    link: [
-      {
-        href: "https://github.com/BrightJun96/react-project/issues/2",
-        text: "이미지 latency 이슈",
-      },
-      {
-        href: "https://github.com/BrightJun96/BlockChain/issues",
-        text: "블록체인 프로젝트 이슈",
-      },
-    ],
   },
-]; /*DOM Selector*/
+];
 
+/*--DOM Selector-- */
 // DOM ID Selector Function
 function domIDSelector(element) {
   return document.getElementById(element);
@@ -138,26 +52,30 @@ const navBar = domIDSelector("navbar");
 const categorys = document.querySelectorAll(".category");
 const moreWatch = domIDSelector("moreWatch");
 const ScrollToTop = domIDSelector("scrollToTop");
-const menuBar = domQuerySelector(".menu-bar");
-const menu = domQuerySelector(".menu");
+const hamburgerBar = domIDSelector("hamburger-bar");
+const menu = domIDSelector("menu");
 
-const navBarHeight = navBar.clientHeight;
-
-/*높이 값을 반환해주는 함수*/
-function sectionHeight(section) {
-  return section.offsetTop - navBarHeight;
-}
-
-/*menuBar toggle*/
-menuBar.addEventListener("click", () => {
+/*menuBar toggle(모바일을 위한 햄버거 바)*/
+hamburgerBar.addEventListener("click", () => {
   menu.classList.toggle("visible");
 });
 
-/*scroll function*/
+/*-- scroll code --*/
+const navBarHeight = navBar.scrollHeight; // offsetHeight, scrollHeight
+console.log(navBarHeight);
+
+// 높이 값을 반환해주는 함수
+function sectionHeight(section) {
+  console.log(section.offsetTop);
+  return section.offsetTop - navBarHeight;
+}
+
+//scroll function
 moreWatch.addEventListener("click", () => {
   scrollTo(0, sectionHeight(domIDSelector("qulifications")));
 });
 
+// To Top
 ScrollToTop.addEventListener("click", () => {
   scrollTo(0, 0);
 });
@@ -180,44 +98,25 @@ categorys.forEach((category) => {
   });
 });
 
-/*modal add function*/
-
+/*-- modal code --*/
 //modal content Selector
 const qulifyBoxes = document.querySelectorAll(".qulify-box");
 const modal = domQuerySelector(".modal");
-const modalRemoveBtn = domQuerySelector(".modal-removeBtn");
+const modalRemoveBtn = domIDSelector("modal-removeBtn");
 const title = domIDSelector("title");
 const description = domIDSelector("description");
-const linkContainer = domIDSelector("link-container");
 const modalDomCollection = [
   { name: title, option: "title" },
   { name: description, option: "description" },
-  // { name: linkContainer, option: "link" },
 ];
 
 // modal content paint function
 function elementPaint(domCollection, index) {
   domCollection.forEach((element) => {
-    // if (element.name === linkContainer) {
-    //   let links = modalContent[index][element.option];
-
-    //   links.forEach((link) => {
-    //     const newLI = document.createElement("li");
-    //     const newA = document.createElement("a");
-    //     newA.href = link.href;
-    //     newA.textContent = link.text;
-    //     newLI.appendChild(newA);
-
-    //     linkContainer.appendChild(newLI);
-    //   });
-    // } else {
-    //   element.name.innerHTML = modalContent[index][element.option];
-    // }
-
     element.name.innerHTML = modalContent[index][element.option];
   });
 }
-//put all modal content
+//open modal
 qulifyBoxes.forEach((box, index) => {
   box.addEventListener("click", () => {
     modal.classList.add("open-modal");
@@ -226,7 +125,7 @@ qulifyBoxes.forEach((box, index) => {
   });
 });
 
-/* remove modal code start*/
+// remove modal
 modalRemoveBtn.addEventListener("click", () => {
   modal.classList.remove("open-modal");
 });
